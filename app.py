c0ecd17a2ff20763fde89c1dbec3e09684d77c36
IIItagaki <157664205+IIItagaki@users.noreply.github.com>
2026-01-21 12:50:59 +0900

    Change return value to '地球'

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def hello() -> str:
     # https://cloud.google.com/run/docs/logging#correlate-logs
     logger.info("Child logger with trace Id.")
 
-    return "Hello, World!"
+    return "地球"
 
 
 def shutdown_handler(signal_int: int, frame: FrameType) -> None:
